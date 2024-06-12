9e398f71329145d13424b84899477049d103d5cd
Thomas Stringer <thomas@trstringer.com>
2024-06-11 20:19:35 -0400

    Update nvim config

--- init.lua
+++ init.lua
@@ -5,6 +5,7 @@
 -- [[ LspConfig ]]
 -- [[ BasicKeymaps ]]
 -- [[ ConfigureLSP ]]
+-- [[ AutoCmd ]]
 
 -- Set <space> as the leader key
 -- See `:help mapleader`
@@ -211,18 +212,69 @@ require('lazy').setup({
     },
   },
 
+  -- {
+  --   -- Theme inspired by Atom
+  --   'navarasu/onedark.nvim',
+  --   priority = 1000,
+  --   lazy = false,
+  --   config = function()
+  --     require('onedark').setup {
+  --       -- Set a style preset. 'dark' is default.
+  --       style = 'dark', -- dark, darker, cool, deep, warm, warmer, light
+  --     }
+  --     require('onedark').load()
+  --   end,
+  -- },
   {
-    -- Theme inspired by Atom
-    'navarasu/onedark.nvim',
-    priority = 1000,
+    'maxmx03/dracula.nvim',
     lazy = false,
+    priority = 1000,
     config = function()
-      require('onedark').setup {
-        -- Set a style preset. 'dark' is default.
-        style = 'dark', -- dark, darker, cool, deep, warm, warmer, light
-      }
-      require('onedark').load()
-    end,
+      ---@type dracula
+      local dracula = require "dracula"
+
+      dracula.setup({
+        transparent = false,
+        on_colors = function(colors, color)
+          ---@type dracula.palette
+          return {
+            -- override or create new colors
+            mycolor = "#ffffff",
+          }
+        end,
+        on_highlights = function(colors, color)
+          ---@type dracula.highlights
+          return {
+            ---@type vim.api.keyset.highlight
+            Normal = { fg = colors.mycolor }
+          }
+        end,
+        plugins = {
+          ["nvim-treesitter"] = true,
+          ["nvim-lspconfig"] = true,
+          ["nvim-navic"] = true,
+          ["nvim-cmp"] = true,
+          ["indent-blankline.nvim"] = true,
+          ["neo-tree.nvim"] = true,
+          ["nvim-tree.lua"] = true,
+          ["which-key.nvim"] = true,
+          ["dashboard-nvim"] = true,
+          ["gitsigns.nvim"] = true,
+          ["neogit"] = true,
+          ["todo-comments.nvim"] = true,
+          ["lazy.nvim"] = true,
+          ["telescope.nvim"] = true,
+          ["noice.nvim"] = true,
+          ["hop.nvim"] = true,
+          ["mini.statusline"] = true,
+          ["mini.tabline"] = true,
+          ["mini.starter"] = true,
+          ["mini.cursorword"] = true,
+        }
+      })
+      vim.cmd.colorscheme 'dracula'
+      vim.cmd.colorscheme 'dracula-soft'
+    end
   },
 
   {
@@ -232,9 +284,12 @@ require('lazy').setup({
     opts = {
       options = {
         icons_enabled = false,
-        theme = 'auto',
+        theme = vim.g.colors_name,
         component_separators = '|',
         section_separators = '',
+        refresh = {
+          statusline = 1000,
+        },
       },
       sections = {
         lualine_c = {
@@ -816,6 +871,16 @@ vim.api.nvim_create_autocmd('BufWritePre', {
     end
 
     vim.lsp.buf.format()
+  end
+})
+
+-- [[ AutoCmd ]]
+vim.api.nvim_create_autocmd("VimLeavePre", {
+  pattern = "*",
+  callback = function()
+    if vim.g.savesession then
+      vim.api.nvim_command("mks!")
+    end
   end
 })
 
